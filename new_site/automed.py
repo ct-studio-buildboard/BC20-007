8b28b69158d134bf5ff4a9fbf68ae86cac163504
Ananya Shivaditya <ananya.shiv@gmail.com>
2020-04-24 11:57:04 -0400

    Modified search

--- new_site/automed.py
+++ new_site/automed.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 
+def search(drug,row):
+    try:
+        if drug in row:
+            return True
+        else:
+            return False
+    except:
+        return False
+
 drug_data = pd.read_csv("substitutes.csv")
 drug_data["Trade_Name"] = drug_data["Trade_Name"].str.lower()
 drug_data["Ingredient"] = drug_data["Ingredient"].str.lower()
+drug_data['Ingredient'] = drug_data['Ingredient'].str.split(';| ')
+drug_data['Name'] = drug_data['Trade_Name'].str.split(';| ')
 
 app = Flask(__name__)
 
@@ -23,10 +34,10 @@ def handle_data():
 		no = []
 
 		for drug in new_pres_drug:
-			a = drug_data['Ingredient'].str.contains(drug)
-			b = drug_data['Trade_Name'].str.contains(drug)
+			a = drug_data.apply(lambda row : search(drug,row['Ingredient']), axis = 1)
+			b = drug_data.apply(lambda row : search(drug,row['Name']), axis = 1)
 
-			substitutes = drug_data[a | b].sort_values('Price')
+			substitutes = drug_data[a | b].sort_values('Price').head(5)
 			n = len(substitutes)
 
 			if n > 0 and drug:
