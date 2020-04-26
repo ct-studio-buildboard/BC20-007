f2b4479a68eea80787fcaa2400554196fff392d4
Avncoolspirit <kumaravnish91@gmail.com>
2020-04-25 21:01:35 -0400

    ocr

--- new_site/automed.py
+++ new_site/automed.py
@@ -1,6 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
 import pandas as pd
 import numpy as np
+import requests
+import json
+from tesserocr import PyTessBaseAPI
+
+
+
+def getOCR(file):
+    with PyTessBaseAPI() as api:
+        api.SetImageFile(file)
+        print(api.GetUTF8Text())
+        api.AllWordConfidences()
+#from flask_socketio import SocketIO, emit, join_room, leave_room
+
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# socketio = SocketIO(app, always_connect=True, engineio_logger=True)
+
+# @socketio.on('connect')
+# def connected():
+#     print('connect')
+    
+# @socketio.on('disconnect')
+# def disconnect():
+#     print('disconnect')
+
+
+def getRxCui (drugName):
+    try:
+        response = requests.get('https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term='+drugName)
+        string = response.content.decode('utf-8')
+        json_obj = json.loads(string)
+        return json_obj['approximateGroup']['candidate'][0]['rxcui']
+    except Exception:
+        return None
+
+def getInteractions (interactionQuery):
+    try:
+        print('checking Interactions',interactionQuery)
+        response = requests.get('https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis='+interactionQuery)
+        string = response.content.decode('utf-8')
+        json_obj = json.loads(string)
+        return json_obj['fullInteractionTypeGroup'][0]['fullInteractionType'][0]['interactionPair'][0]['description']
+    except Exception:
+        return None
+
 
 def search(drug,row):
     try:
@@ -17,26 +61,39 @@ drug_data["Ingredient"] = drug_data["Ingredient"].str.lower()
 drug_data['Ingredient'] = drug_data['Ingredient'].str.split(';| ')
 drug_data['Name'] = drug_data['Trade_Name'].str.split(';| ')
 
+
+
 app = Flask(__name__)
+
+# @socketio.on('image-upload')
+# def imageUpload(image):
+#     emit('send-image', image, broadcast = True)
+
 
 @app.route("/")
 def home():
-	return render_template("automed_test.html")
+    return render_template("automed_test.html")
+    # return render_template("automed_main.html")
+
+
+@app.route('/uploadPres' , methods=['POST'])
+def get_image():
+	file = request.files['imgFile'].read() ## byte file
+	getOCR(file)
+	return jsonify({'status':"SUCCESS"})
 
 @app.route('/handle_data', methods=['POST','GET'])
 def handle_data():
-	if request.method == "POST":
-		
+	if request.method == "POST":		
 		new_pres_drug = request.form['drug']
 		new_pres_drug = new_pres_drug.lower().split(",")
-
 		result = []
 		no = []
-
-		for drug in new_pres_drug:
+		interaction=[]
+		for drug in new_pres_drug:  
+			interaction.append(getRxCui(drug))
 			a = drug_data.apply(lambda row : search(drug,row['Ingredient']), axis = 1)
 			b = drug_data.apply(lambda row : search(drug,row['Name']), axis = 1)
-
 			substitutes = drug_data[a | b].sort_values('Price').head(5)
 			n = len(substitutes)
 
@@ -46,8 +103,11 @@ def handle_data():
 				result.append({'name':drug.upper(),'num':n,'s':subs,'p':prices})
 			else:
 				no.append(drug)
-
+		print(getInteractions("+".join(interaction)))
 		return render_template("automed_success.html", result = result,no=no)
 
+
+
 if __name__ == "__main__":
-    app.run('0.0.0.0')(debug=False)
+    app.run()(debug=True)
+    #socketio.run(app, debug=True)
