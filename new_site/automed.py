6d1f9577ea6590e86d718c4cb0f8a692f0a9e4c0
Ubuntu <ubuntu@ip-172-31-15-156.us-east-2.compute.internal>
2020-04-26 03:45:26 +0000

    implment ocr

--- new_site/automed.py
+++ new_site/automed.py
@@ -4,14 +4,35 @@ import numpy as np
 import requests
 import json
 from tesserocr import PyTessBaseAPI
+import sys
+import spacy  #pip install -U spacy
+import scispacy  #pip install scispacy
+import en_ner_bc5cdr_md  #pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.4/en_ner_bc5cdr_md-0.2.4.tar.gz
+import re
 
+nlp = en_ner_bc5cdr_md.load()
+def detect_drugs(text):
+	#NLP model for drug recognition
+	#nlp = en_ner_bc5cdr_md.load()
+	#the input text file
+	#use NLP model to parse the text
+
+	parsed_text = nlp(text)
+	#extract entities/ drug names
+	entities = parsed_text.ents
+	#print entities
+	print("List of entities:", entities)
+	return entities
 
 
 def getOCR():
+    extract = ''
     with PyTessBaseAPI() as api:
         api.SetImageFile('pres.jpg')
-        print(api.GetUTF8Text())
+        extract=detect_drugs(api.GetUTF8Text())
         api.AllWordConfidences()
+    search =' '.join([token.orth_ for token in extract])
+    return re.findall(r'[a-zA-Z]+',search)
 #from flask_socketio import SocketIO, emit, join_room, leave_room
 
 # app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -80,8 +101,7 @@ def home():
 def get_image():
 	file = request.files['imgFile'] ## byte file
 	file.save('pres.jpg')
-	getOCR()
-	return jsonify({'status':"SUCCESS"})
+	return handle_data_backend(getOCR())
 
 @app.route('/handle_data', methods=['POST','GET'])
 def handle_data():
@@ -106,6 +126,26 @@ def handle_data():
 				no.append(drug)
 		print(getInteractions("+".join(interaction)))
 		return render_template("automed_success.html", result = result,no=no)
+
+def handle_data_backend(new_pres_drug):	
+	result = []
+	no = []
+	#interaction=[]		
+	for drug in new_pres_drug:  
+		#interaction.append(getRxCui(drug))
+		a = drug_data.apply(lambda row : search(drug.lower(),row['Ingredient']), axis = 1)
+		b = drug_data.apply(lambda row : search(drug.lower(),row['Name']), axis = 1)
+		substitutes = drug_data[a | b].sort_values('Price').head(5)
+		n = len(substitutes)
+
+		if n > 0 and drug:
+			prices = list(substitutes['Price'])
+			subs = list(substitutes['Trade_Name'])
+			result.append({'name':drug.upper(),'num':n,'s':subs,'p':prices})
+		# else:
+		# 	no.append(drug.lower())
+	#print(getInteractions("+".join(interaction)))
+	return render_template("automed_success.html", result = result,no=no)
 
 
 
