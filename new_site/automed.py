7815d7ca1f38e3cf30a550bf64735efbdae5236d
Zwee Dao Thai Duy <zweedaothaiduy@Zwees-MacBook-Pro.local>
2020-04-27 18:57:18 -0400

    partial drug name search using levenshtein distance

--- new_site/automed.py
+++ new_site/automed.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
+import Levenshtein as lev #pip install python-levenshtein
 
 def search(drug,row):
     try:
         if drug in row:
             return True
         else:
+            for word in row:
+                #Levenshtein distance ratio
+                ratio = lev.ratio(drug, word)
+                if ratio > 0.8:
+                    return True
             return False
     except:
         return False
+
+def correct_drug_name(drug, names):
+    for row in names:
+        if drug in row:
+            return drug
+        else:
+            for word in row:
+                #Levenshtein distance ratio
+                ratio = lev.ratio(drug, word)
+                if ratio > 0.8:
+                    return word
+            return False
+
 
 drug_data = pd.read_csv("substitutes.csv")
 drug_data["Trade_Name"] = drug_data["Trade_Name"].str.lower()
@@ -21,33 +40,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("automed_test.html")
+    return render_template("automed_test.html")
 
 @app.route('/handle_data', methods=['POST','GET'])
 def handle_data():
-	if request.method == "POST":
-		
-		new_pres_drug = request.form['drug']
-		new_pres_drug = new_pres_drug.lower().split(",")
+    if request.method == "POST":
+        
+        new_pres_drug = request.form['drug']
+        new_pres_drug = new_pres_drug.lower().split(",")
 
-		result = []
-		no = []
+        result = []
+        no = []
 
-		for drug in new_pres_drug:
-			a = drug_data.apply(lambda row : search(drug,row['Ingredient']), axis = 1)
-			b = drug_data.apply(lambda row : search(drug,row['Name']), axis = 1)
+        for drug in new_pres_drug:
+            a = drug_data.apply(lambda row : search(drug,row['Ingredient']), axis = 1)
+            b = drug_data.apply(lambda row : search(drug,row['Name']), axis = 1)
 
-			substitutes = drug_data[a | b].sort_values('Price').head(5)
-			n = len(substitutes)
+            substitutes = drug_data[a | b].sort_values('Price').head(5)
+            n = len(substitutes)
 
-			if n > 0 and drug:
-				prices = list(substitutes['Price'])
-				subs = list(substitutes['Trade_Name'])
-				result.append({'name':drug.upper(),'num':n,'s':subs,'p':prices})
-			else:
-				no.append(drug)
+            if n > 0 and drug:
+                #find correct drug name
+                drug_name = correct_drug_name(drug, substitutes['Name'])
+                if not drug_name:
+                    drug_name = correct_drug_name(drug, substitutes['Ingredient'])
 
-		return render_template("automed_success.html", result = result,no=no)
+                #print results of alternatives
+                prices = list(substitutes['Price'])
+                subs = list(substitutes['Trade_Name'])
+                result.append({'name':drug_name.upper(),'num':n,'s':subs,'p':prices})
+            else:
+                no.append(drug)
+
+        return render_template("automed_success.html", result = result,no=no)
 
 if __name__ == "__main__":
     app.run('0.0.0.0')(debug=False)
